0287d685ab0522526f478aba1519347623514a5c
Vasilen Minkov <vadlsilenminkov@gmail.com>
2022-05-10 09:49:59 +0300

    Delete useless files, add Axios and call API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const API_KEY = 'b1cd543fab32ccde7564f7940c3ca1c8'
 
-function App() {
+export default function App() {
+  const [data, setData] = useState({});
+  const [error, setError] = useState(false);
+  const [lat, setLat] = useState("51.509865");
+  const [lon, setLon] = useState("-0.118092");
+
+  useEffect(() => { 
+    const fetchDataFromAPI = async () => {
+      try {
+        const result = await axios.get(
+          `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
+        );
+        if ( result && result.status === 200 && result.data &&  result.data.list &&  result.data.list.length ) {
+          setData(result.data);
+        }
+      } catch (err) {
+        setError(err);
+      }
+    };
+    fetchDataFromAPI();
+  },[])
+  if(!data || !Object.keys(data)) return null;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <h1>{`Weather in ${data.city.name}`}</h1>
     </div>
   );
 }
-
-export default App;
